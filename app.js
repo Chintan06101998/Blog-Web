aad68cce4067c21c8458a496271737c5cede6f09
= <=>
2022-12-26 14:48:59 -0500

    TODO: "EJS files added" WIP: Create forms

--- app.js
+++ app.js
@@ -4,17 +4,22 @@ const express = require("express");
 
 const app = express();
 
+// TODO: register views Engine
+app.set('views engine','ejs');
+//app.set('viewss', 'myviewsa') //myviews means path that contain your views
+
+
 //TODO: listen for request
 app.listen(3000);
 
 //TODO: send response as a HTML
 app.get('/',(req,res)=>{
     //res.send('<h1>WEE</h2>');
-    res.sendFile('./view/index.html',{root: __dirname});  // by default express look for computers path so we need to mentioned directory of path
+    res.sendFile('./views/index.html',{root: __dirname});  // by default express look for computers path so we need to mentioned directory of path
 });
 
 app.get('/about',(req,res)=>{
-    res.sendFile('./view/about.html',{root: __dirname}); 
+    res.sendFile('./views/about.html',{root: __dirname}); 
    // res.send('<h1>About Page</h2>');
 });
 
@@ -26,5 +31,5 @@ app.get('/about-us',(req,res)=>{
 //TODO: 404 page  : use fun for every single request doesnot matter what URL it is
 
 app.use((req,res)=>{      // note that place it in last because it fire for every request
-    res.statusCode(404).sendFile('./view/PageError.html',{root: __dirname})
+    res.statusCode(404).sendFile('./views/PageError.html',{root: __dirname})
 })
