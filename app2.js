d2c21fee1c844764807c174074f9b87832470001
= <=>
2022-12-26 21:54:26 -0500

    Used: Custom Middleware WIP: Morgan middleware

--- app2.js
+++ app2.js
@@ -1,10 +1,29 @@
 const express = require("express");
+const morgan = require('morgan');
 
 //TODO: Express app
 
 const app = express();
 
 app.listen(3000);
+
+//TODO: create middleware by using USE method
+
+/*
+//TODO: it is custom middle ware .. in next, we use morgan for static middleware
+
+app.use((req,res, next)=>{  // next is used to tell browser that what ypu have to do after execution of this code
+    console.log('New request made: ');
+    console.log('host: ',req.hostname);
+    console.log('path: ',req.path);
+    console.log('method: ',req.method);
+    next(); //  // next is used to tell browser that what ypu have to do after execution of this code
+});
+
+*/
+
+app.use(morgan('dev'));
+
 
 // TODO: register View Engine
 app.set('view engine','ejs');
@@ -20,6 +39,14 @@ app.get('/',(req,res)=>{
     res.render('index',{title:'Home',blogs});
 });
 
+/*
+app.use((req,res, next)=>{  // next is used to tell browser that what ypu have to do after execution of this code
+    console.log('in the next middleware: ');
+
+    next(); //  // next is used to tell browser that what ypu have to do after execution of this code
+}); */
+
+
 app.get('/about',(req,res)=>{
     //res.send('<h1>WEE</h2>');
     res.render('about',{title:'about'});
@@ -30,10 +57,10 @@ app.get('/about-us',(req,res)=>{
     res.redirect('/about');
 });
 
-app.get('/create',(req,res)=>{
+app.get('/create',(req,res)=>{   //TODO: it is only used for get request and give spefific file
     res.render('create',{title:'Create a new blog'});
 })
 
-app.use((req,res)=>{
+app.use((req,res)=>{   // TODO: USE method run for any request doesnot matter it is post or get method
     res.status(404).render('404',{title:'404'});
 })
